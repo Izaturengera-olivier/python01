da41f71b0d52378fbcc31e464b926df638928177
IZATURENGERA Olivier <izzyy720@gmail.com>
2023-11-02 11:16:16 +0200

    new commit

---
+++ main.py
@@ -0,0 +1,23 @@
+num1=48
+num2=5984
+result=num1+num2
+print("the sum of",num1,"and",num2,"is:",result)
+original_string=input("enter the string:")
+print("the original string is:",original_string)
+print("the reversed string is:",original_string[::-1])
+num_str="42"
+num_int=int(num_str)
+print(num_int)
+print(type(num_int))
+num_float=3.14
+num_str2=str(num_float)
+print(num_str2)
+print(type(num_str2))
+name=input("what is your name?")
+age=int(input("how old are you?"))
+print("hello",name,"you are",age,"years old")
+str1="string"
+str2="shuffle"
+print(str1[::-1])
+print(str2[::-1])
+print(str1[::-1] + str2[::-1])
